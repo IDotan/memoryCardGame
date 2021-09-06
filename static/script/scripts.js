1d050c87b9f163e67c28e67ac42a1f22b81e8040
Itai <62836633+IDotan@users.noreply.github.com>
2021-09-06 09:21:12 +0300

    share link function

--- static/script/scripts.js
+++ static/script/scripts.js
@@ -1,3 +1,28 @@
-function test() {
-    console.log("test")
+const user_data = {
+    name: "bob",
+    mail: "e@mail.com",
+    score: -1,
+    time: 9999999
+};
+
+function share_score() {
+    let url = document.URL + "?" + user_data.name + "&" + user_data.mail + "&" + user_data.score + "&" + user_data.time;
+    return url;
+};
+
+function shared_link() {
+    let url = document.URL;
+    if (url.includes("?")) {
+        let url_array = url.split("?")[1].split("&");
+        user_data.name = url_array[0];
+        user_data.mail = url_array[1];
+        user_data.score = parseInt(url_array[2]);
+        user_data.time = parseInt(url_array[3]);
+    };
+    // call the function to show the end game screen
 }
+
+
+window.onload = () => {
+    shared_link();
+};
