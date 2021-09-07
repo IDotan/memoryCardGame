4c83bfa24fe879187063ebb60aa5fd473ba2bdec
BlackTiger145 <62836633+BlackTiger145@users.noreply.github.com>
2021-09-07 11:56:14 +0300

    simplify some js and added documentation

--- static/script/scripts.js
+++ static/script/scripts.js
@@ -5,27 +5,82 @@ const user_data = {
     time: 9999999
 };
 
+/**
+ * Show the relevent page section while hiding all the others.
+ * 
+ * @param {Number} show section DOM "index" to show. * 
+ */
+function section_switch(show) {
+    let sections_array = Array.from(document.getElementsByClassName("page_section"))
+    sections_array.forEach((section, index) => {
+        if (index == show) {
+            section.classList.remove('hide');
+        } else {
+            section.classList.add('hide');
+        };
+    });
+};
+
+/**
+ * Create url link to share with the user data.
+ * 
+ * @returns url link
+ */
 function share_score() {
     let url = document.URL + "?" + user_data.name + "&" + user_data.mail + "&" + user_data.score + "&" + user_data.time;
     return url;
 };
 
+/**
+ * Sort table data by score, from lowest to largest.
+ * 
+ * @param {Array} a data array to check.
+ * @param {Array} b data array to check.
+ * @returns a[2] - b[2]
+ */
 function sort_score_ascending(a, b) {
     return a[2] - b[2];
 };
 
+/**
+ * Sort table data by score, from largest to lowest.
+ * 
+ * @param {Array} a data array to check.
+ * @param {Array} b data array to check.
+ * @returns b[2] - a[2]
+ */
 function sort_score_descending(a, b) {
     return b[2] - a[2];
 };
 
+/**
+ * Sort table data by time, from shortest to longest.
+ * 
+ * @param {Array} a data array to check.
+ * @param {Array} b data array to check.
+ * @returns a[3] - b[3]
+ */
 function sort_time_ascending(a, b) {
     return a[3] - b[3];
 };
 
+/**
+ * Sort table data by time, from longest to shortest.
+ * 
+ * @param {Array} a data array to check.
+ * @param {Array} b data array to check.
+ * @returns b[3] - a[3]
+ */
 function sort_time_descending(a, b) {
     return b[3] - a[3];
 };
 
+
+/**
+ * Load the sorted data in to the end screen table.
+ * 
+ * @param {Function} sort_function the function to sort by.
+ */
 function load_score_table(sort_function) {
     let table_data = [["itai", "itai145@gmail.com", 8, 30000]];
     table_data.push([user_data.name, user_data.mail, user_data.score, user_data.time]);
@@ -37,18 +92,34 @@ function load_score_table(sort_function) {
     document.getElementById('score_table').innerHTML = document.getElementById('score_table').innerHTML.split("</tr>", 1)[0] + "</tr>" + str_data;
 };
 
-function table_arrow_switch(show, hide) {
-    document.getElementsByClassName("table_arrow")[show].classList.remove("hide");
-    document.getElementsByClassName("table_arrow")[hide].classList.add("hide");
-    document.getElementsByClassName("table_arrow")[hide].classList.add("up");
+/**
+ * Show the wanted arrow while hiding any other arrow.
+ * 
+ * @param {Number} show arrow "index" in the DOM to show.
+ */
+function table_arrow_switch(show) {
+    let arrows_array = Array.from(document.getElementsByClassName("table_arrow"));
+    arrows_array.forEach((arrow, index) => {
+        if (index == show) {
+            arrow.classList.remove("hide");
+        } else {
+            arrow.classList.add("hide");
+            arrow.classList.add("up");
+        };
+    });
 };
 
+/**
+ * Sort the score table by the table header clicked.
+ * 
+ * @param {Object} event click event object.
+ */
 function table_sort_click(event) {
     let clicked_on = event.target.innerText
     let up = event.target.children[0].classList.contains("up")
     switch (clicked_on) {
         case "Score":
-            table_arrow_switch(0, 1);
+            table_arrow_switch(0);
             if (up) {
                 event.target.children[0].classList.remove("up");
                 load_score_table(sort_score_descending);
@@ -58,7 +129,7 @@ function table_sort_click(event) {
             }
             break;
         case "Time":
-            table_arrow_switch(1, 0);
+            table_arrow_switch(1);
             if (up) {
                 event.target.children[0].classList.remove("up");
                 load_score_table(sort_time_ascending);
@@ -70,6 +141,9 @@ function table_sort_click(event) {
     };
 };
 
+/**
+ * Run when useing shared link, load user info and show the end screen table.
+ */
 function shared_link() {
     let url = document.URL;
     if (url.includes("?")) {
@@ -80,6 +154,7 @@ function shared_link() {
             user_data.score = parseInt(url_array[2]);
             user_data.time = parseInt(url_array[3]);
             load_score_table(sort_score_descending);
+            section_switch(2);
         };
     };
 };
